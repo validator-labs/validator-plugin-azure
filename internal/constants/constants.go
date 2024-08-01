42386e2fe50157e7e92e3aff897e68bd0ab4b3ae
Matt Welke <matt.welke@spectrocloud.com>
2024-08-01 15:41:56 -0400

    Remove unused constant.
    Signed-off-by: Matt Welke <matt.welke@spectrocloud.com>

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -5,9 +5,6 @@ const (
 	// PluginCode is the code for the plugin.
 	PluginCode string = "Azure"
 
-	// ValidationTypeRBAC is the validation type for RBAC rules.
-	ValidationTypeRBAC string = "azure-rbac"
-
 	// ValidationTypeRBACRole is the validation type for RBAC role rules.
 	ValidationTypeRBACRole string = "azure-rbac-role"
 
